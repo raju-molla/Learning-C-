b5c7f419b213f3b94cf074944f0b499cab636dd6
rajuQtec <rajumolla.qtec@gmail.com>
2025-01-04 03:38:53 +0600

    Switch case when operator

--- Program.cs
+++ Program.cs
@@ -1,6 +1,19 @@
 ﻿using System;
 class Test{
     public static void Main(String[] args) {
-        Console.WriteLine(123);
+
+      int number;
+      Console.WriteLine("Enter a number between 1 to 10");
+      number = Convert.ToInt32(Console.ReadLine());
+      string message = number switch{
+         int num when num < 1 || num > 10 => "number is out of range",
+         int num when num%2==0 => "Even",
+         int num when num%2!=0 => "odd",
+         _ => "default"
+
+      };
+      Console.WriteLine(message);
+
+
     }
 }
