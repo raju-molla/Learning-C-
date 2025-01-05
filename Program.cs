83323aae9709fdd08142cadced0e76b9b39ab4df
rajuQtec <rajumolla.qtec@gmail.com>
2025-01-06 01:47:05 +0600

    Constructor

--- Program.cs
+++ Program.cs
@@ -3,10 +3,15 @@
 class Person{
     public string name;
     public int age;
-    public void SetValue(string setName, int setAge){
+
+    public Person(string setName, int setAge){
         name = setName;
         age = setAge;
     }
+    // public void SetValue(string setName, int setAge){
+    //     name = setName;
+    //     age = setAge;
+    // }
     public void Display(){
         Console.WriteLine($"Name: {name}, Age: {age}");
     }
@@ -15,13 +20,11 @@ class Test
 {
     public static void Main(String[] args)
     {
-        Person p1 = new Person();
-        p1.SetValue("RajuMolla", 25);
+        Person p1 = new Person("Raju Molla", 25);
         p1.Display();
 
 
-        Person p2 = new Person();
-        p2.SetValue("Rijvi", 30);
+        Person p2 = new Person("Rijvi", 30);
         p2.Display();
 
        
